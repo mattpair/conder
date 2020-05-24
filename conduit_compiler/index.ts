9512ffbd77e05642b17536876918b2445096b882
Jeremy Drouillard <jadrouil@umich.edu>
2020-05-24 12:06:50 -0700

    First iteration of reading files

--- conduit_compiler/index.ts
+++ conduit_compiler/index.ts
@@ -1,1 +1,10 @@
-import fs from 'fs';
+/// <reference types="node" />
+
+import * as fs from 'fs';
+
+try {
+    const dir: string[] = fs.readdirSync("./conduit/")
+    console.log("success")
+} catch(e) {
+    console.error("Unable to find conduit files in conduit/", e)
+}
