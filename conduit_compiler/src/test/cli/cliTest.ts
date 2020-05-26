0595116461ce784237dab9ada2c9d4cbecb112a2
Jeremy Drouillard <jadrouil@umich.edu>
2020-05-25 17:53:59 -0700

    pkg compiler

--- conduit_compiler/src/test/cli/cliTest.ts
+++ conduit_compiler/src/test/cli/cliTest.ts
@@ -2,7 +2,7 @@ import * as child_process from "child_process";
 import * as fs from "fs";
 
 test("invoking compiler without dir fails", () => {
-  const r = child_process.execSync("node ../../../dist/index.js 2>&1", {
+  const r = child_process.execSync("./conduit 2>&1", {
     cwd: "src/test/cli/",
     encoding: "utf-8",
   });
@@ -44,7 +44,7 @@ describe.each(testDirs.map(dir => dir.name))(
 
     it("\nprotos", () => {
       const out = child_process.execSync(
-        "node ../../../../dist/index.js 2>&1",
+        "../conduit 2>&1",
         {
           cwd: testDir,
           encoding: "utf-8",
