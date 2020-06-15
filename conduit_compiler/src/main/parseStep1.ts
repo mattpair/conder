498802861e20b7ac705a3e1e9525abab403fba1c
jermdroui <jerm@conder.systems>
2020-06-15 13:13:31 -0700

    Pull common if statement up

--- conduit_compiler/src/main/parseStep1.ts
+++ conduit_compiler/src/main/parseStep1.ts
@@ -6,7 +6,6 @@ import {BaseConduitFile, Enum, EntityLocation, BaseField, BaseMsg, BaseImport, E
 
 export namespace Parse {
     export type File = BaseConduitFile<Message, Enum, Import>
-    type MatchResult = {hit: true, match: RegExpExecArray, loc: EntityLocation} | {hit: false}    
     
     export type FieldType = Classified<"primitive", PrimitiveUnion> | Classified<"deferred", {from?: string, type: string}>
     export type Field = BaseField<FieldType>
@@ -20,51 +19,47 @@ export namespace Parse {
     type CompleteParser = ParserTreeNode<File>
     const completeParse: CompleteParser = {
         kind: "composite",
-        parseStart(c: FileCursor): File | undefined {
-            return {kind: EntityKind.File, loc: c.filelocation, children: {Enum: [], Message: [], Import: []}}
+        startRegex: /^/,
+        parseStart(c: RegExpExecArray): WithoutLocation<File> | undefined {
+            return {kind: EntityKind.File, children: {Enum: [], Message: [], Import: []}}
         },
+        endRegex: /^\s*/,
         parseEnd(c): IsEndParseSuccessful {
-            return c.tryMatch(/^\s*/).hit && c.isDone() ? "success" : "fail"
+            return c.length > 0 ? "success" : "fail"
         },
         sub: {
             Message: {
                 kind: "composite",
-                parseStart(c: FileCursor): Message | undefined {
-                    const result = c.tryMatch(/^\s*message +(?<name>[a-zA-Z_]\w*) *{/)
-                    if(result.hit) {
-                        return {
-                            kind: EntityKind.Message,
-                            loc: result.loc,
-                            name: result.match.groups.name,
-                            children: {
-                                Field: []
-                            }
+                startRegex: /^\s*message +(?<name>[a-zA-Z_]\w*) *{/,
+                parseStart(c: RegExpExecArray): WithoutLocation<Message> | undefined {
+                    return {
+                        kind: EntityKind.Message,
+                        name: c.groups.name,
+                        children: {
+                            Field: []
                         }
                     }
                 },
-                
-                parseEnd(c: FileCursor): IsEndParseSuccessful {
-                    return c.tryMatch(/^\s*}/).hit ? "success" : "fail"
-                    
+                endRegex:/^\s*}/,
+                parseEnd(c: RegExpExecArray): IsEndParseSuccessful {
+                    return c.length > 0 ? "success" : "fail"
                 },
+
                 sub: {
                     Field: {
                         kind: "leaf",
-                        parse(c: FileCursor): Field | undefined {
-                            const result = c.tryMatch(/^\s*(?<optional>optional)? +((?<from>[_A-Za-z]+[\w]*)\.)?(?<type>[_A-Za-z]+[\w]*) +(?<name>[_A-Za-z]+[\w]*)(,|\n)/)
-                            if (result.hit) {
+                        regex: /^\s*(?<optional>optional)? +((?<from>[_A-Za-z]+[\w]*)\.)?(?<type>[_A-Za-z]+[\w]*) +(?<name>[_A-Za-z]+[\w]*)(,|\n)/,
+                        parse(c: RegExpExecArray): WithoutLocation<Field> | undefined {
 
-                                const prim = Primitives.find(p => p === result.match.groups.type)
-                                return {
-                                    kind: EntityKind.Field,
-                                    loc: result.loc,
-                                    name: result.match.groups.name,
-                                    isRequired: result.match.groups.optional === undefined,
-                                    fType: prim !== undefined ? {kind: "primitive", val: prim} : {kind: "deferred", val: {
-                                        from: result.match.groups.from,
-                                        type: result.match.groups.type
-                                    }}
-                                }
+                            const prim = Primitives.find(p => p === c.groups.type)
+                            return {
+                                kind: EntityKind.Field,
+                                name: c.groups.name,
+                                isRequired: c.groups.optional === undefined,
+                                fType: prim !== undefined ? {kind: "primitive", val: prim} : {kind: "deferred", val: {
+                                    from: c.groups.from,
+                                    type: c.groups.type
+                                }}
                             }
                         }
                     }
@@ -72,33 +67,28 @@ export namespace Parse {
             },
             Enum: {
                 kind: "composite",
-                parseStart(c: FileCursor): Enum | undefined {
-                    const result = c.tryMatch(/^\s*enum +(?<name>[a-zA-Z_]\w*) *{/)
-                    if (result.hit) {
-                        return {
-                            kind: EntityKind.Enum,
-                            loc: result.loc,
-                            name: result.match.groups.name,
-                            children: {
-                                EnumMember: []
-                            }
+                startRegex: /^\s*enum +(?<name>[a-zA-Z_]\w*) *{/,
+                parseStart(c: RegExpExecArray): WithoutLocation<Enum> | undefined {
+                    return {
+                        kind: EntityKind.Enum,
+                        name: c.groups.name,
+                        children: {
+                            EnumMember: []
                         }
                     }
                 },
-                parseEnd(c: FileCursor): IsEndParseSuccessful {
-                    return c.tryMatch(/^\s*}/).hit ? "success": "fail"
+                endRegex:/^\s*}/,
+                parseEnd(c: RegExpExecArray): IsEndParseSuccessful {
+                    return c.length > 0 ? "success": "fail"
                 },
                 sub: {
                     EnumMember: {
                         kind: "leaf",
-                        parse(c: FileCursor): EnumMember | undefined {
-                            const result = c.tryMatch(/^\s*(?<name>[a-zA-Z_]\w*)(,|\s)/)
-                            if (result.hit) {
-                                return{
-                                    kind: EntityKind.EnumMember,
-                                    loc: result.loc,
-                                    name: result.match.groups.name
-                                }
+                        regex: /^\s*(?<name>[a-zA-Z_]\w*)(,|\s)/,
+                        parse(c: RegExpExecArray): WithoutLocation<EnumMember> | undefined {
+                            return{
+                                kind: EntityKind.EnumMember,
+                                name: c.groups.name
                             }   
                         }
                     }
@@ -106,16 +96,13 @@ export namespace Parse {
             },
             Import: {
                 kind: "leaf",
-                parse(c: FileCursor): Import | undefined {
-                    const res = c.tryMatch(/^\s*import +'(?<presentDir>\.\/)?(?<location>[\w \.\/]*)' +as +(?<name>[_A-Za-z]+[\w]*)/)
-                    if(res.hit) {
-                        return {
-                            kind: EntityKind.Import,
-                            loc: res.loc,
-                            fromPresentDir: res.match.groups.presentDir !== undefined,
-                            name: res.match.groups.name,
-                            filename: res.match.groups.location
-                        }
+                regex: /^\s*import +'(?<presentDir>\.\/)?(?<location>[\w \.\/]*)' +as +(?<name>[_A-Za-z]+[\w]*)/,
+                parse(c: RegExpExecArray): WithoutLocation<Import> | undefined {
+                    return {
+                        kind: EntityKind.Import,
+                        fromPresentDir: c.groups.presentDir !== undefined,
+                        name: c.groups.name,
+                        filename: c.groups.location
                     }
                 }
             }
@@ -123,6 +110,8 @@ export namespace Parse {
     }
 
     const symbolRegex: RegExp = new RegExp(`^(${Object.values(Symbol).join("|")})`)
+
+    type MatchResult = {hit: true, match: RegExpExecArray, loc: EntityLocation} | {hit: false}
     class FileCursor {
         private absOffset=0
         private line=0
@@ -186,18 +175,26 @@ export namespace Parse {
         
     export function extractAllFileEntities(contents: string, location: FileLocation): File {
         const cursor = new FileCursor(contents, location)
-        return extractToCompositeEntity(cursor, completeParse)
+        const f: WithoutLocation<File> = extractToCompositeEntity(cursor, completeParse)
+        return Object.assign(f, {
+            loc: cursor.filelocation
+        })
     }
 
     type ChildOf<K extends WithChildren, CHILD_TYPE extends AnyEntity> = Extract<CHILD_TYPE, {kind: keyof K["children"]}>
 
     type W = ChildOf<File, WithChildren>
 
-    function extractToCompositeEntity<K extends WithChildren>(cursor: FileCursor, parser: CompositeParserNode<K>): K | undefined {
-        const k: K = parser.parseStart(cursor)
-        if (k === undefined) {
+
+    function extractToCompositeEntity<K extends WithChildren>(cursor: FileCursor, parser: CompositeParserNode<K>): K extends File ? WithoutLocation<File> : K | undefined {
+        const m = cursor.tryMatch(parser.startRegex)
+        if (!m.hit) {
             return undefined
         }
+        const withoutLoc = parser.parseStart(m.match)
+        // @ts-ignore
+        const k: K extends File ? WithoutLocation<File> : K = withoutLoc.kind === EntityKind.File ? withoutLoc : Object.assign(withoutLoc, {loc: m.loc})
+        
         let tryExtractChild = true 
         while (tryExtractChild) {
             tryExtractChild = false
@@ -214,8 +211,9 @@ export namespace Parse {
                         }
                         break
                     case "leaf":
-                        const leaf = c.parse(cursor)
-                        if (leaf !== undefined) {
+                        const match = cursor.tryMatch(c.regex)
+                        if (match.hit) {
+                            const leaf = c.parse(match.match)
                             //@ts-ignore
                             k.children[key].push(leaf)
                             tryExtractChild = true
@@ -228,8 +226,8 @@ export namespace Parse {
                 }
             }
         }
-
-        if (parser.parseEnd(cursor) === "success") {
+        const end = cursor.tryMatch(parser.endRegex)
+        if (end.hit) {
             return k
         }
 
@@ -247,12 +245,18 @@ export namespace Parse {
     };
 
     type CompositeParserNode<ROOT extends WithChildren> = {
-        parseStart(c: FileCursor): ROOT | undefined
-        parseEnd(c: FileCursor): IsEndParseSuccessful
+        startRegex: RegExp
+        parseStart(c: RegExpExecArray): WithoutLocation<ROOT> | undefined
+        parseEnd(c: RegExpExecArray): IsEndParseSuccessful
+        endRegex: RegExp
         sub: ParserTree<ROOT>,
     } & ParserNode<"composite">
 
     type ParserNode<KIND> = {kind: KIND}
-    type LeafParserNode<ROOT extends AnyEntity> =  ParserNode<"leaf"> & { parse(c: FileCursor): ROOT | undefined}
+    type LeafParserNode<ROOT extends AnyEntity> =  ParserNode<"leaf"> & { 
+        regex: RegExp
+        parse(c: RegExpExecArray): WithoutLocation<ROOT> | undefined
+    }
     type ParserTreeNode<ROOT extends AnyEntity> = ROOT extends WithChildren ? CompositeParserNode<ROOT> : LeafParserNode<ROOT>
+    type WithoutLocation<K extends AnyEntity> = Omit<K, "loc">
 }
