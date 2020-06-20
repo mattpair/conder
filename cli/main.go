bbfba9945f0290b9e79e788b19a25be18d08c46c
jermdroui <jerm@conder.systems>
2020-06-19 20:34:28 -0700

    http2 connection from go wip

--- cli/main.go
+++ cli/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "./cmd"
+import (
+	// "./cmd"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
-	cmd.Execute()
+	out, err := http.Get("https://localhost:7344")
+	if err != nil {
+		fmt.Println("error", err)
+		return
+	}
+	defer out.Body.Close()
+	if out != nil {
+		fmt.Println(out)
+	}
+	body, err := ioutil.ReadAll(out.Body)
+
+	if err != nil {
+		fmt.Println("error", err)
+	}
+	if body != nil {
+		fmt.Println(out)
+	}
+
+	// cmd.Execute()
 }
--- conduit_compiler/src/main/server.ts
+++ conduit_compiler/src/main/server.ts
@@ -1,7 +1,21 @@
-import * as http from 'http';
+import * as http2 from 'http2';
+import * as fs from 'fs';
 
-http.createServer((req, res) => {
+const server = http2.createSecureServer({
+    key: fs.readFileSync('localhost-privkey.pem'),
+    cert: fs.readFileSync('localhost-cert.pem')
+});
+
+server.on("request", (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("hello, jeremy")
     res.end()
-}).listen(7344)
+})
+
+server.on("stream", (stream) => {
+    stream.respond({ ':status': 200, 'content-type': 'text/plain' });
+    stream.write('hello ');
+    stream.end('world');
+})
+
+server.listen(7344)
