f0913bc661f031837c15aa40311d6f5adcb3a504
jermdroui <jerm@conder.systems>
2020-06-23 08:20:42 -0700

    Generate terraform deployment instructions and execute

--- conduit_compiler/src/main/conduit.ts
+++ conduit_compiler/src/main/conduit.ts
@@ -183,8 +183,61 @@ EXPOSE  8080
 
 ENTRYPOINT [ "python3", "manage.py", "runserver", "0.0.0.0:8080" ]
 `)
+    fs.writeFileSync(".deploy/cloud-run.tf",
+`
+provider "google" {
+    project = "conder-systems-281115"
+    region = "us-west1"
+    zone = "a"
+    version = "~> 3.26"
+}
+
+resource "google_cloud_run_service" "genservice" {
+  name     = "cloudrun-srv"
+  location = "us-west1"
+
+  template {
+    spec {
+      containers {
+        image = "us.gcr.io/conder-systems-281115/hello-world-gen"
+      }
+    }
+  }
+
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale"      = "2"
+        "run.googleapis.com/client-name"        = "terraform"
+      }
+    }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.genservice.location
+  project     = google_cloud_run_service.genservice.project
+  service     = google_cloud_run_service.genservice.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+`)
     child_process.execSync("docker build -t conder-systems/cloud-run-gen . ", {cwd: ".deploy/"})
     child_process.execSync("docker tag conder-systems/cloud-run-gen us.gcr.io/conder-systems-281115/hello-world-gen", {cwd: ".deploy/"})
+    child_process.execSync("docker push us.gcr.io/conder-systems-281115/hello-world-gen")
+    child_process.execSync("terraform init && terraform apply -auto-approve", {cwd: ".deploy/", encoding: "utf-8"})
 }
 
 function main() {
