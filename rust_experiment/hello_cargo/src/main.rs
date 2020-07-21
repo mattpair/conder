48d532dd0099c39508ff6b4e00e337265879f7fd
jermdroui <jerm@conder.systems>
2020-07-21 14:46:29 -0700

    Containerize server

--- rust_experiment/hello_cargo/src/main.rs
+++ rust_experiment/hello_cargo/src/main.rs
@@ -23,7 +23,7 @@ async fn main() -> std::io::Result<()> {
             .route("/ret", web::get().to(return_something))
             .route("/ingest", web::post().to(ingest_handler))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
