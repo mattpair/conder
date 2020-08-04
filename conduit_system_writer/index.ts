d76d2bf3acf92a28ec2c8c6d6144384967c2b433
jermdroui <jerm@conder.systems>
2020-08-04 14:06:36 -0400

    Move system writer to separate project

---
+++ conduit_system_writer/index.ts
@@ -0,0 +1,11 @@
+import { writeRustAndContainerCode } from './src/main/compute/gcp/server_writer';
+import { generateAllClients } from './src/main/models/generate';
+import { generateModels } from "./src/main/models/generate";
+
+
+export {
+    generateModels,
+    generateAllClients,
+    writeRustAndContainerCode
+}
+
---
+++ conduit_system_writer/src/main/config/load.ts
@@ -0,0 +1,29 @@
+import * as fs from 'fs';
+import { StepDefinition } from "../util/sequence";
+
+export type ConduitBuildConfig = {
+    project: string,
+    dependents?: {
+        [path in string]: {
+            language: "typescript"
+        }
+    }
+}
+
+export const loadBuildConfig: StepDefinition<{}, {buildConf: ConduitBuildConfig}> = {
+    stepName: "loadBuildConfig",
+    func: () => {
+        if (fs.existsSync("cdtconfig.json")) {
+            const raw = fs.readFileSync("cdtconfig.json", {encoding: "utf-8"})
+            // TODO: actually validate.
+            const buildConf: ConduitBuildConfig = JSON.parse(raw)
+            if (!buildConf.dependents) {
+                console.warn("Please specify a dependent in your conduit config so I know where to put models.")
+                process.exit(1)
+            }
+            return Promise.resolve({buildConf})
+        } else {
+            return Promise.reject("Unable to find config file (cdtconfig.json) in present directory.")
+        }
+    }
+}
