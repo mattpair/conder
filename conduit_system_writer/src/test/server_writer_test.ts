d9f0e2752db60e285a2f087099d82a1c54485696
jermdroui <jerm@conder.systems>
2020-08-04 16:46:20 -0400

    Manifest Builder for test writing

--- conduit_system_writer/src/test/server_writer_test.ts
+++ conduit_system_writer/src/test/server_writer_test.ts
@@ -1,6 +1,89 @@
 import { writeRustAndContainerCode } from "../main/server_writer"
 import { CompiledTypes, Lexicon } from "conduit_compiler"
 
+class StructBuilder {
+    readonly name: string
+    readonly fields: CompiledTypes.Field[]
+    readonly parent: ManifestBuilder
+    constructor(name: string, parent: ManifestBuilder) {
+        this.name = name
+        this.fields = []
+        this.parent = parent
+    }
+
+    addPrimitiveField(name: string, isRequired: boolean, type: Lexicon.PrimitiveUnion): this {
+        this.fields.push({
+            kind: "Field",
+            loc: ManifestBuilder.loc,
+            isRequired,
+            name,
+            part: {
+                FieldType: {
+                    kind: "FieldType",
+                    differentiate: () => ({kind: "Primitive", loc: ManifestBuilder.loc, val: type})
+                }
+            }
+        })
+        return this
+    }
+
+    build(): CompiledTypes.Struct {
+        const struct: CompiledTypes.Struct ={
+            kind: "Struct",
+            file: {dir: "", name: "", fullname: ""},
+            loc: ManifestBuilder.loc,
+            children: {
+                Field: [
+                    {
+                        loc: ManifestBuilder.loc, 
+                        kind: "Field", 
+                        isRequired: true,
+                        name: "onlyField",
+    
+                        part: {
+                            FieldType: {
+                                kind: "FieldType",
+                                differentiate: () => ({loc: ManifestBuilder.loc, kind: "Primitive", val: Lexicon.Symbol.bool})
+                                
+                            }
+                        }
+                    }]
+            },
+            name: "test"
+        }
+
+        this.parent.map.set(this.name, struct)
+        return struct
+    }
+    
+}
+
+
+class ManifestBuilder {
+    static readonly loc = {startColNumber: 0, startLineNumber: 0, endColNumber: 0, endLineNumber: 0}
+    static readonly file = {dir: "", name: "", fullname: ""}
+    readonly map = new Map()
+
+    
+    struct(name: string): StructBuilder {
+        return new StructBuilder(name, this)
+    }
+
+    build(): CompiledTypes.Manifest {
+
+        return {
+            namespace: {
+                name: "default",
+                inScope: new CompiledTypes.EntityMap(this.map)
+            },
+            service: {
+                kind: "public",
+                functions: []
+            }
+        }
+    }
+}
+
 async function runCodeGenTest(manifest: CompiledTypes.Manifest): Promise<void> {
     const r = await writeRustAndContainerCode.func({manifest})
     const mainFiles = r.backend.main.files.filter(f => !(/Cargo/.test(f.name)))
@@ -24,41 +107,9 @@ test("empty everything", async () => {
 
 
 test("only one struct", async () => {
-    const loc = {startColNumber: 0, startLineNumber: 0, endColNumber: 0, endLineNumber: 0}
-    const struct: CompiledTypes.Struct = {
-        kind: "Struct",
-        file: {dir: "", name: "", fullname: ""},
-        loc,
-        children: {
-            Field: [
-                {
-                    loc, 
-                    kind: "Field", 
-                    isRequired: true,
-                    name: "onlyField",
+    const entBuilder = new ManifestBuilder()
+    entBuilder.struct("test").addPrimitiveField("onlyField", true, Lexicon.Symbol.bool).build()
+    
 
-                    part: {
-                        FieldType: {
-                            kind: "FieldType",
-                            differentiate: () => ({loc, kind: "Primitive", val: Lexicon.Symbol.bool})
-                            
-                        }
-                    }
-                }]
-        },
-        name: "test"
-    }
-    const map = new Map()
-    map.set("test", struct)
-
-    runCodeGenTest({
-        namespace: {
-            name: "default",
-            inScope: new CompiledTypes.EntityMap(map)
-        },
-        service: {
-            kind: "public",
-            functions: []
-        }
-    })
+    runCodeGenTest(entBuilder.build())
 })
