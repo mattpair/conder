9f7a8230e17ea57f34364b316ab1e7e30059b10e
jermdroui <jerm@conder.systems>
2020-08-04 16:18:37 -0400

    test simple struct

--- conduit_system_writer/src/test/server_writer_test.ts
+++ conduit_system_writer/src/test/server_writer_test.ts
@@ -1,5 +1,5 @@
 import { writeRustAndContainerCode } from "../main/server_writer"
-import { CompiledTypes } from "conduit_compiler"
+import { CompiledTypes, Lexicon } from "conduit_compiler"
 
 async function runCodeGenTest(manifest: CompiledTypes.Manifest): Promise<void> {
     const r = await writeRustAndContainerCode.func({manifest})
@@ -14,6 +14,47 @@ test("empty everything", async () => {
         namespace: {
             name: "default",
             inScope: new CompiledTypes.EntityMap(new Map())
+        },
+        service: {
+            kind: "public",
+            functions: []
+        }
+    })
+})
+
+
+test("only one struct", async () => {
+    const loc = {startColNumber: 0, startLineNumber: 0, endColNumber: 0, endLineNumber: 0}
+    const struct: CompiledTypes.Struct = {
+        kind: "Struct",
+        file: {dir: "", name: "", fullname: ""},
+        loc,
+        children: {
+            Field: [
+                {
+                    loc, 
+                    kind: "Field", 
+                    isRequired: true,
+                    name: "onlyField",
+
+                    part: {
+                        FieldType: {
+                            kind: "FieldType",
+                            differentiate: () => ({loc, kind: "Primitive", val: Lexicon.Symbol.bool})
+                            
+                        }
+                    }
+                }]
+        },
+        name: "test"
+    }
+    const map = new Map()
+    map.set("test", struct)
+
+    runCodeGenTest({
+        namespace: {
+            name: "default",
+            inScope: new CompiledTypes.EntityMap(map)
         },
         service: {
             kind: "public",
