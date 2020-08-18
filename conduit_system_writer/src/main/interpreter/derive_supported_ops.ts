08188cfd33780ea21bb2633ff40e9c8c50b083a7
jermdroui <jerm@conder.systems>
2020-08-18 16:51:55 -0500

    Move opdef to op factory

--- conduit_system_writer/src/main/interpreter/derive_supported_ops.ts
+++ conduit_system_writer/src/main/interpreter/derive_supported_ops.ts
@@ -1,7 +1,6 @@
 import { CompiledTypes, Lexicon, Utilities} from 'conduit_compiler';
 import {generateInsertRustCode, generateRustGetAllQuerySpec, createSQLFor, generateQueryInterpreter} from '../sql'
 import { assertNever } from 'conduit_compiler/dist/src/main/utils';
-import { writeOperationInterpreter } from './interpreter_writer';
 
 
 export interface OpDef {
@@ -22,9 +21,59 @@ export interface OpFactory {
     makeQuery(store: CompiledTypes.HierarchicalStore): OpInstance
 }
 
-export const deriveSupportedOperations: Utilities.StepDefinition<{manifest: CompiledTypes.Manifest}, {supportedOps: OpDef[]}> = {
+class TheOpFactory implements OpFactory {
+
+    makeInsert(store: CompiledTypes.HierarchicalStore, varname: string): OpInstance {
+        return undefined
+    }
+
+    makeQuery(store: CompiledTypes.HierarchicalStore): OpInstance {
+        return undefined
+    }
+
+    inferStoreOps(store: CompiledTypes.HierarchicalStore): OpDef[] {
+        const insertOp = {
+            rustOpHandler: `
+            Op::Insert_${store.name}(insert_var_name) => {
+                let to_insert = match state.get(insert_var_name).unwrap() {
+                    AnyType::${store.typeName}(r) => r,
+                    _ => panic!("invalid insertion type")
+                };
+
+                match insert_${store.name}(&client, &to_insert).await {
+                    Ok(()) => AnyType::None,
+                    Err(err) => AnyType::Err(err.to_string())
+                }
+            }`,
+            rustEnumMember: `Insert_${store.name}(String)`
+        }
+        const queryOp = {
+            rustEnumMember: `Query_${store.name}(${store.specName})`,
+            rustOpHandler: `Op::Query_${store.name}(spec) => {
+                match query_interpreter_${store.name}(spec, &client).await {
+                    Ok(out) => AnyType::${store.name}Result(out),
+                    Err(err) => AnyType::Err(err.to_string())
+                }
+            }`
+        }
+
+        return [insertOp, queryOp]
+    }
+
+    makeReturnVariableOp(varname: string): OpInstance {
+        return undefined
+    }
+
+    makeReturnPrevious(): OpInstance {
+        return undefined
+    }
+}
+
+export const deriveSupportedOperations: Utilities.StepDefinition<{manifest: CompiledTypes.Manifest}, {supportedOps: OpDef[], opFactory: OpFactory}> = {
     stepName: "deriving supported operations",
     func: ({manifest}) => {
+
+        const opFactory = new TheOpFactory()
         const addedOperations: OpDef[] = [
             {
                 rustEnumMember: `Return(String)`,
@@ -52,39 +101,13 @@ export const deriveSupportedOperations: Utilities.StepDefinition<{manifest: Comp
                 case "Struct":
                     break
     
-                case "HierarchicalStore":
-                    const store = i                
-                
-                    addedOperations.push({
-                        rustOpHandler: `
-                        Op::Insert_${store.name}(insert_var_name) => {
-                            let to_insert = match state.get(insert_var_name).unwrap() {
-                                AnyType::${store.typeName}(r) => r,
-                                _ => panic!("invalid insertion type")
-                            };
-
-                            match insert_${store.name}(&client, &to_insert).await {
-                                Ok(()) => AnyType::None,
-                                Err(err) => AnyType::Err(err.to_string())
-                            }
-                        }`,
-                        rustEnumMember: `Insert_${store.name}(String)`
-                    })
-
-                    addedOperations.push({
-                        rustEnumMember: `Query_${store.name}(${store.specName})`,
-                        rustOpHandler: `Op::Query_${store.name}(spec) => {
-                            match query_interpreter_${store.name}(spec, &client).await {
-                                Ok(out) => AnyType::${store.name}Result(out),
-                                Err(err) => AnyType::Err(err.to_string())
-                            }
-                        }`
-                    })
+                case "HierarchicalStore":                
+                    addedOperations.push(...opFactory.inferStoreOps(i))
                     break
                 default: assertNever(i)
             }
         })
 
-        return Promise.resolve({supportedOps: addedOperations })
+        return Promise.resolve({supportedOps: addedOperations, opFactory})
     }
 }
