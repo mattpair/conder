cb8a8c85e4dc9f9076b3758d86d6528e56a13e6a
jermdroui <jerm@conder.systems>
2020-09-14 17:01:25 -0400

    schema for store query should be simple

--- conduit_src/conduit_parser/src/main/resolution/generateSystemStructs.ts
+++ conduit_src/conduit_parser/src/main/resolution/generateSystemStructs.ts
@@ -29,29 +29,29 @@ export function generateSystemObjects(entityMapInternal: PartialEntityMap): Part
 function generateQuerySpec(store: HierarchicalStore): Struct {
 
     const fields: Parse.Field[] = []
-    function toQuerySpec(s: AnySchemaInstance): AnySchemaInstance {
-        switch(s.kind) {
-            case "Array":
-                return schemaFactory.Array(toQuerySpec(s.data[0]))
-            case "Object":
-                const spec: SchemaInstance<"Object"> = {kind: "Object", data: {}}
-                for (const key in s.data) {
-                    spec.data[key] = toQuerySpec(s.data[key])
-                }
-                return spec
-            case "Optional":
-                return schemaFactory.Optional(toQuerySpec(s.data[0]))
-            case Symbol.double:
-            case Symbol.bool:
-            case Symbol.int:
-            case Symbol.string:
-                return s
+    // function toQuerySpec(s: AnySchemaInstance): AnySchemaInstance {
+    //     switch(s.kind) {
+    //         case "Array":
+    //             return schemaFactory.Array(toQuerySpec(s.data[0]))
+    //         case "Object":
+    //             const spec: SchemaInstance<"Object"> = {kind: "Object", data: {}}
+    //             for (const key in s.data) {
+    //                 spec.data[key] = toQuerySpec(s.data[key])
+    //             }
+    //             return spec
+    //         case "Optional":
+    //             return schemaFactory.Optional(toQuerySpec(s.data[0]))
+    //         case Symbol.double:
+    //         case Symbol.bool:
+    //         case Symbol.int:
+    //         case Symbol.string:
+    //             return s
                 
-                default: assertNever(s)
-        }
-    }
+    //             default: assertNever(s)
+    //     }
+    // }
 
-    const schema = toQuerySpec(store.schema) as SchemaInstance<"Object">
+    const schema: SchemaInstance<"Object"> = schemaFactory.Object({getAll: schemaFactory.bool})
     
     
     return {
