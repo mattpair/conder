cc62a84e3b70a15100f34a93472cf96fd79c1a04
jermdroui <jerm@conder.systems>
2020-10-01 18:00:47 -0700

    Use pulumi to provision user's infra

--- conduit_src/conduit_kernel/index.ts
+++ conduit_src/conduit_kernel/index.ts
@@ -13,7 +13,7 @@ module.exports.writeMain = writeMain
 module.exports.containerize = function () {
     writeMain()
     console.log("PWD", process.cwd())
-    child_process.execSync(`docker build -t kernel-server .`, {cwd: "./src/rust", stdio: "inherit"})
+    child_process.execSync(`docker build -t us.gcr.io/conder-systems-281115/kernel-server:latest . && docker push us.gcr.io/conder-systems-281115/kernel-server:latest`, {cwd: "./src/rust", stdio: "inherit"})
 }
 
 export function getOpWriter(): CompleteOpWriter {
