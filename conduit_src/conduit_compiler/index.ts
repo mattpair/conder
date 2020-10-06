7b14311f7bc8e2c59c1991fc6ec7338381b09592
jermdroui <jerm@conder.systems>
2020-10-05 20:50:29 -0700

    Better containerize

--- conduit_src/conduit_compiler/index.ts
+++ conduit_src/conduit_compiler/index.ts
@@ -1,4 +1,21 @@
+import { compileFiles } from 'conduit_parser';
 
+import {compile} from './src/main/statement_converter'
+import {StrongServerEnv, RequiredEnv,} from 'conduit_kernel'
 
-export {compile} from './src/main/statement_converter'
-export {generateClients} from './src/main/clients'
+export {StrongServerEnv, RequiredEnv, Var, ServerEnv} from 'conduit_kernel'
+export type CompileResponse = {
+    kind: "success", env: Pick<StrongServerEnv, RequiredEnv>
+  } | 
+  {
+    kind: "error", reason: string
+  }
+  
+export function string_to_environment(conduit: string): CompileResponse {
+    try {
+        const manifest = compileFiles({input: () => conduit}, {project: "singleton", dependents: {}})
+        return {kind: "success", env: compile(manifest)} 
+    } catch (e) {
+        return {kind: "error", reason: e.message}
+    }
+}
