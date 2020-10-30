4e3f796b17b73ab8368ee2495201324fc6f5f6e5
jermdroui <jerm@conder.systems>
2020-10-30 09:46:29 -0700

    Make fewer assumptions in test

--- conder_core/src/test/dag_to_instruction.ts
+++ conder_core/src/test/dag_to_instruction.ts
@@ -1,12 +1,17 @@
 
-import {Test, schemaFactory} from 'conder_kernel'
+import {Test, schemaFactory, AnyOpInstance} from 'conder_kernel'
 import {Node, to_instruction} from '../../index'
 
 describe("basic functionality", () => {
     const TEST_STORE = "testStore"
-    type DagServer = {call: () => Promise<any>}
-    function testHarness(node: Node, test: (server: DagServer) => Promise<void>): jest.ProvidesCallback {
-        const ops = to_instruction(node)
+    type DagServer = Record<string, (...arg: any[]) => Promise<any>>
+
+    function testHarness(proc_nodes: Record<string,Node>, test: (server: DagServer) => Promise<void>): jest.ProvidesCallback {
+        const PROCEDURES: Record<string, AnyOpInstance[]> = {}
+        for (const key in proc_nodes) {
+            PROCEDURES[key] = to_instruction(proc_nodes[key])
+        }
+
         const STORES = {TEST_STORE: schemaFactory.Object({})}
         return (cb) => {
         
@@ -15,11 +20,15 @@ describe("basic functionality", () => {
                     MONGO_CONNECTION_URI: `mongodb://localhost:${mongo.port}`,
                     SCHEMAS: [],
                     DEPLOYMENT_NAME: "statefultest",
-                    PROCEDURES: {func: ops},
+                    PROCEDURES,
                     STORES
                 }, "./conder_kernel/")
                 .then(server => {
-                    return test({call: () => server.invoke("func")}).finally(() => {
+                    const testSurface: DagServer = {}
+                    for (const key in PROCEDURES) {
+                        testSurface[key] = () => server.invoke(key)
+                    }
+                    return test(testSurface).finally(() => {
                         cb()
                         server.kill()
                     })
@@ -29,14 +38,16 @@ describe("basic functionality", () => {
         }
     }
                 
-    it("can return a field access", 
-        testHarness({
-            kind: "select",
-            store: TEST_STORE,
-            after: {kind: "return"}
+    it("allows selections", 
+        testHarness({select:
+            {
+                kind: "select",
+                store: TEST_STORE,
+                after: {kind: "return"}
+            }
         },
         async (server) => {
-            const res = await server.call()
+            const res = await server.select()
             expect(res).toEqual([])
         })
     )
