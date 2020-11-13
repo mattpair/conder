527445e26765d1a694624b00d374bb3f14cf32e4
jermdroui <jerm@conder.systems>
2020-11-13 14:21:03 -0800

    Rename compile to semantics

--- tuna-lang/src/main/assembled.ts
+++ tuna-lang/src/main/assembled.ts
@@ -1,6 +1,6 @@
 import {FUNCTION_TRANSFORMER, FunctionDescription, Transformer ,Compiler} from 'conder_core'
 import {Parser} from './parser'
-import {semantify, Manifest} from './compile'
+import {semantify, Manifest} from './semantics'
 
 export const TUNA_TO_MANIFEST = new Transformer<string, Manifest>(str => {
     const p = new Parser(str).parse()
