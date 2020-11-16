b9aeb2769d4b76a479e3ac3fb8a0af90669029b1
Jeremy Drouillard <jadrouil@umich.edu>
2020-11-16 14:29:38 -0800

    Update release to include asset bundle

--- release.sh
+++ release.sh
@@ -1,9 +1,14 @@
 #!/bin/zsh
-export CONDER_VERSION=0.0.2
+export CONDER_VERSION=0.0.3
 
-cd conder_core/conder_kernel/
+cd conder_core/conder_kernel &&
 npm run gen && 
-cd src/rust/ && 
+cd src/rust/ &&
 docker build -t condersystems/sps:$CONDER_VERSION . && 
 docker push condersystems/sps:$CONDER_VERSION &&
-gh release create v$CONDER_VERSION -p
+cd ../../../ &&
+npm run compile &&
+cd .. &&
+tar --exclude='**/node_modules/' --exclude='**/rust/' -czhf conder-api.tar.gz conder_core
+gh release create v$CONDER_VERSION -p -t v$CONDER_VERSION 'conder-api.tar.gz#Conder API'
+rm conder-api.tar.gz
