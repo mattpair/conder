8aab74564d9e7a00a77fefae1901313b27865647
Jeremy Drouillard <jadrouil@umich.edu>
2020-11-16 16:08:34 -0800

    Release single package

--- release.sh
+++ release.sh
@@ -1,15 +1,14 @@
 #!/bin/zsh
-export CONDER_VERSION=0.0.5
+export CONDER_VERSION=0.0.6
 
-cd conder_core/conder_kernel
-npm run gen 
-cd src/rust/
+cd conder_core/
+npm run compile
+cd src/main/ops/rust/
 docker build -t condersystems/sps:$CONDER_VERSION . 
 docker push condersystems/sps:$CONDER_VERSION
-cd ../../../
-npm run compile
+cd ../../../../
 node -p "JSON.stringify({...require('./package.json'), version: '$CONDER_VERSION'}, null, 2)" > temp.json && mv temp.json package.json
 cd ..
-tar --exclude='**/rust/' -czhf conder-api.tar.gz conder_core
+tar --exclude='**/rust/' --exclude='**/node_modules/' --exclude='conder_core/src/' --exclude='**/*.spec.*/' -czhf conder-api.tar.gz conder_core
 gh release create v$CONDER_VERSION -p -t v$CONDER_VERSION 'conder-api.tar.gz#Conder API'
 rm conder-api.tar.gz
