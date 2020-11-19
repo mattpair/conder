6dfb5c9b89e70561807cf486420931a24cad6d69
Jeremy Drouillard <jadrouil@umich.edu>
2020-11-18 18:59:31 -0800

    Allows getting data multiple times in sequence iff no sets

--- conder_core/src/main/abstract/temporal_logic/logic.spec.ts
+++ conder_core/src/main/abstract/temporal_logic/logic.spec.ts
@@ -1,14 +1,21 @@
-import { validate } from "./main"
-
+import { ActionSequence, validate } from "./main";
 
 describe("temporal logic", () => {
+  it("allows getting the same piece of data twice if it cannot be mutated", () => {
+    const gets: ActionSequence = [
+      { kind: "get", id: "i" },
+      { kind: "get", id: "i" },
+    ];
+    expect(validate({ gets })).toEqual([]);
 
-
-    it("allows getting the same piece of data twice if it cannot be mutated", () => {
-        expect(validate([
-            {kind: "get", id: "i"},
-            {kind: "get", id: "i"}
-        ], new Map())).toEqual({kind :"success"})
-    })
-
-})
+    expect(validate({ gets, sets: [{ kind: "set", id: "i" }] }))
+      .toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "func": "gets",
+          "msg": "Getting i multiple times while mutated elsewhere",
+        },
+      ]
+    `);
+  });
+});
--- conder_core/src/main/abstract/temporal_logic/main.ts
+++ conder_core/src/main/abstract/temporal_logic/main.ts
@@ -1,16 +1,52 @@
 type Actions = {
     get: {id: string},
-    set: {id: string}
+    set: {id: string},
 }
+type ActionKind = keyof Actions
 type AnyAction = {
     [K in keyof Actions]: {kind: K} & Actions[K]
 }[keyof Actions]
 
 
 export type ActionSequence = AnyAction[]
-export type Result = {kind: "error", message: string} | {kind: "success"}
+type Error = { msg: string, func: string}
 
-export function validate(seq: ActionSequence, meanwhile: ReadonlyMap<string, ActionSequence>): Result {
+export function validate(sequences: Record<string, ActionSequence>): Error[] {
+    const actionAgainstData = new Map<string, Set<ActionKind>>()
+    
 
-    return {kind: "success"}
+    Object.values(sequences).forEach(seq => {
+        seq.forEach(action => {
+            const ops = actionAgainstData.get(action.id)
+            if (ops === undefined) {
+                actionAgainstData.set(action.id, new Set<ActionKind>([action.kind]))
+            } else {
+                ops.add(action.kind)
+                actionAgainstData.set(action.id, ops)
+            }
+        })
+    })
+    const errors: Error[] = []
+    Object.keys(sequences).forEach(func => {
+        const previouslyGot = new Set<string>()
+        sequences[func].forEach(action => {
+            
+            switch (action.kind) {
+                case "get":
+                    if (previouslyGot.has(action.id)) {
+                        if (actionAgainstData.get(action.id).has("set")) {
+                            errors.push({msg: `Getting ${action.id} multiple times while mutated elsewhere`, func})
+                        }
+                    } else {
+                        previouslyGot.add(action.id)
+                    }
+                case "set":
+                    break
+                default: 
+                    const n: never = action
+            }
+        })
+    })
+
+    return errors
 }
